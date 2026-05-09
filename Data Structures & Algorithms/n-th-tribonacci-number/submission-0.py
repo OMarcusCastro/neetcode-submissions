3871cf3fa3c6c217457fc7451618ed17b292e5de
Marcus Castro <51335230+OMarcusCastro@users.noreply.github.com>
2026-05-09 17:54:12 -0300

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        t  = deque()
+        t.append(0)
+        t.append(1)
+        t.append(1)
+
+
+
+        if n<3:
+            return t[n]
+
+        for i in range(2,n):
+            print(t)
+            t0 = t.popleft()
+            t.append(t0+t[0]+t[1])
+
+        print(t[2])
+        return t[2]
